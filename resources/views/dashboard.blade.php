5b00ec1d803a725e4696fc2e27d6a3d326278d92
rage1985 <sascha.schaal@valora.net>
2022-08-31 10:15:45 +0200

    Add Table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,4 +8,22 @@
 
     </div>
 
+    <div class="container">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">Artikelbezeichnung</th>
+                <th scope="col">Artikelnummer</th>
+                <th scope="col">Artikelbezeichnung BW</th>
+                <th scope="col">Funktionen</th>
+            </tr>
+            </thead>
+        </table>
+    </div>
+    <script>
+        $(document).ready( function () {
+            $('#Artikel').DataTable();
+        } );
+    </script>
+
 @endsection
