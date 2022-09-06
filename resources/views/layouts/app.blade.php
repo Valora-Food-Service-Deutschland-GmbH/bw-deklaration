f6c0e7bf62d5e5ca43c09c02969bd1dc2e035aaa
rage1985 <sascha.schaal@valora.net>
2022-09-06 15:47:49 +0200

    Footer unten an Seite fixiert. Schrift verkleinert.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,13 +42,13 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
-<footer class="text-center mt-auto navbar-light bg-light" >
+<footer class="text-center mt-auto navbar-light bg-light fixed-bottom" >
     <!-- Grid container -->
     <div class="container p-4"></div>
     <!-- Grid container -->
 
     <!-- Copyright -->
-    <div class="text-center p-3" >
+    <div class="text-center p-1" >
         © 2022 Copyright: Valora Food Service Deutschland GmbH
     </div>
     <!-- Copyright -->
