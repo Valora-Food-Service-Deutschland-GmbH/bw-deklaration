342997b05498e02f759b667b9e57b528b59a5e41
rage1985 <sascha.schaal@valora.net>
2022-09-06 16:12:44 +0200

    Remove Table from Dashboard. Dashboard will eventually be removed as Landing Page as soon as Artikel Views are ready.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,26 +3,7 @@
 @section('content')
     <div class="container">
 
-        <h1>Test Tabelle</h1>
+        <h1>Dashboard</h1>
 
     </div>
-
-    <div class="container">
-        <table class="table table-striped table-bordered table-hover" id="#Artikel">
-            <thead>
-            <tr>
-                <th scope="col">Artikelbezeichnung</th>
-                <th scope="col">Artikelnummer</th>
-                <th scope="col">Artikelbezeichnung BW</th>
-                <th scope="col">Funktionen</th>
-            </tr>
-            </thead>
-        </table>
-    </div>
-    <script>
-        $(document).ready( function () {
-            $('#Artikel').DataTable();
-        } );
-    </script>
-
 @endsection
