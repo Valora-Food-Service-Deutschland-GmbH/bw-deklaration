bb0f03d8dc803019ae1d20718eb6201b820b2d14
rage1985 <sascha.schaal@valora.net>
2022-09-07 14:07:20 +0200

    Refactoration of Routes.

--- routes/web.php
+++ routes/web.php
@@ -37,28 +37,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 });
 
-Route::get('/bla', function () {
-    return view('bla');
-});
-
-Route::controller(UserController::class)->group(function () {
-    Route::get('/user', 'index')->name('user.index');
-    Route::get('/user/{id}','show')->name('user.show');
-    Route::get('user/{id}/edit','edit')->name('user.edit');
-    Route::post('/user/{id}/update','update')->name('user.update');
-    Route::get('/user/create', 'create')->name('user.create');
-    Route::post('/user/store','store')->name('user.store');
-    Route::post('/user/{id}/destroy,','destroy')->name('user.destroy');
-});
-
-Route::controller(ArtikelController::class)->group(function () {
-    Route::get('/artikel', 'index')->name('artikel.index');
-    Route::get('/artikel/{id}','show')->name('artikel.show');
-    Route::get('/artikel/{id}/edit','edit')->name('artikel.edit');
-    Route::post('/artikel/{id}/update','update')->name('artikel.update');
-    Route::get('/artikel/create', 'create')->name('artikel.create');
-    Route::post('/artikel/store','store')->name('artikel.store');
-    Route::post('/artikel/{id}/destroy,','destroy')->name('artikel.destroy');
-    Route::get('/artikel/makelabel','makelabel')->name('artikel.makelabel');
-    Route::post('/artikel/download','download')->name('artikel.download');
-});
+Route::resources([
+    'artikel' => \App\Http\Controllers\Artikel::class,
+    'users' => \App\Http\Controllers\UserController::class,
+]);
