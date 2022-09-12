e0a5e569d92e7582b33f715c1ddca5d4ed1b103f
rage1985 <sascha.schaal@valora.net>
2022-09-12 14:33:57 +0200

    Added colums.

--- resources/views/Artikel/index.blade.php
+++ resources/views/Artikel/index.blade.php
@@ -12,9 +12,22 @@
             <thead>
             <tr>
                 <th scope="col">ID</th>
-                <th scope="col">Name</th>
-                <th scope="col">email</th>
-                <th scope="col">Funktionen</th>
+                <th scope="col">address</th>
+                <th scope="col">ingredients</th>
+                <th scope="col">fat</th>
+                <th scope="col">fattyacids</th>
+                <th scope="col">carbs</th>
+                <th scope="col">sugar</th>
+                <th scope="col">protein</th>
+                <th scope="col">salt</th>
+                <th scope="col">traces</th>
+                <th scope="col">article_id</th>
+                <th scope="col">partner_id</th>
+                <th scope="col">store_id</th>
+                <th scope="col">burn_kj</th>
+                <th scope="col">burn_ckal</th>
+                <th scope="col">weight</th>
+                <th scope="col">actions</th>
             </tr>
             </thead>
         </table>
@@ -23,6 +36,11 @@
         </a>
     </div>
     <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
         $(document).ready( function () {
             $('#Artikel').DataTable({
                 'processing': 'false',
