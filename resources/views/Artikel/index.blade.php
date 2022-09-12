5fa8360ce2beba9fdd1e38957f09432029e39c59
rage1985 <sascha.schaal@valora.net>
2022-09-12 10:49:11 +0200

    Changed Order of JavaScript includes. Changed Datatables Script.

--- resources/views/Artikel/index.blade.php
+++ resources/views/Artikel/index.blade.php
@@ -25,39 +25,15 @@
     <script>
         $(document).ready( function () {
             $('#Artikel').DataTable({
-                'processing': 'true',
-                'serverSide': 'true',
+                'processing': 'false',
+                'serverSide': 'false',
                 'ajax': {
                     'url': 'https://deklaration.valora.shop/artikel/ajax',
                     'method': 'GET',
-                    'dataSrc': 'data',
-                    'error': function (jqXHR, testStatus, errorThrown){
-                        $('#Artikel').Datatable().clear().draw();
-                    }
+
                 },
-                'columns': [{
-                    'data': 'DT_RowIndex',
-                    'name': 'DT_RowIndex'
-                },
-                    {
-                        'data': 'name',
-                        'name': 'name'
-                    },
-                    {
-                        'data': 'email',
-                        'name': 'email'
-                    },
-                    {
-                        'data': 'created_at',
-                        'name': 'created_at'
-                    },
-                    {
-                        'data': 'action',
-                        'name': 'action',
-                        'orderable': true,
-                        'searchable': true
-                    },
-                ]
+
+
             });
 
         });
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,8 +15,14 @@
     <!-- Styles -->
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <link href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css" rel='stylesheet' type='text/css'>
+    <link href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css" rel='stylesheet' type='text/css'>
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+
+    <!-- JS Inculdes -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
     <style>
         body {
@@ -38,9 +44,7 @@
     @yield('content')
 
     <!-- JavaScripts -->
-<script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-{{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+
 </body>
 <footer class="text-center mt-auto navbar-light bg-light fixed-bottom" >
     <!-- Grid container -->
