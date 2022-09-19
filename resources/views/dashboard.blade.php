d55639f3017419e2cfbf72968b1903ee6aeb0858
rage1985 <sascha.schaal@valora.net>
2022-09-19 16:18:39 +0200

    Show Azure Data on Dashboard.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,13 @@
     <div class="container">
 
         <h1>Dashboard</h1>
+        @unless (Auth::check())
+            <p>You are not signed in.</p>
+        @endunless
+
+        @isset($profile)
+            {{dd($profile)}}
+        @endisset
 
     </div>
 @endsection
