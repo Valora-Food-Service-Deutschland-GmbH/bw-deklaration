3aab8e537be542aea2fd3a16c262bd1edae9420e
rage1985 <sascha.schaal@valora.net>
2022-09-20 12:56:45 +0200

    Change User Table to reflect AzureAD.

---
+++ database/migrations/2022_09_20_124944_alter_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('name', 'displayname');
+            $table->string('firstname')->nullable();
+            $table->string('givenname')->nullable();
+            $table->boolean('admin');
+            $table->integer('partner_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
